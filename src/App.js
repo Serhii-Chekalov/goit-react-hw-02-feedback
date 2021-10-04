9c575767a59053afb0c25f312d8a3cc90e03609c
Serhii Chekalov <78024336+Serhii-Chekalov@users.noreply.github.com>
2021-10-04 06:54:17 +0300

    add_components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React, { Component } from "react";
+import { Section } from "./components/Section/Section.jsx";
+import { Statistic } from "./components/Statistics/Statistics.jsx";
+import { Button } from "./components/FeedbackOptions/FeedbackOptions.jsx";
+import { Notification } from "./components/Notification/Notification.jsx";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  render() {
+    const { good, neutral, bad } = this.state;
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <Button
+            options={["good", "neutral", "bad"]}
+            onLeaveFeedback={this.handleIncrement}
+          />
+        </Section>
+        {good || neutral || bad > 0 ? (
+          <Section title="Statistic">
+            <Statistic
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={this.countTotalFeedback()}
+              positivePercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        ) : (
+          <Notification message="No feedback given"></Notification>
+        )}
+      </div>
+    );
+  }
 }
 
 export default App;
